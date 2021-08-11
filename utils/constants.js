de06f149500272efab77147712f41e3d16bde4df
Denis Belkin <Kers001@yandex.ru>
2021-08-11 15:21:49 +0300

    image display fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,23 +1,23 @@
 export const initialCards = [
     {
         name: 'Сочи',
-        link: '../images/polyana.jpg'
+        link: './images/polyana.jpg'
     },
     {
         name: 'Казань',
-        link: '../images/kazan.jpg'
+        link: './images/kazan.jpg'
     },
     {
         name: 'Нижний Новгород',
-        link: '../images/nizhniy.jpg'
+        link: './images/nizhniy.jpg'
     },
     {
         name: 'Иваново',
-        link: '../images/ivanovo.jpg'
+        link: './images/ivanovo.jpg'
     },
     {
         name: 'Москва',
-        link: '../images/metro.jpg'
+        link: './images/metro.jpg'
     },
     {
         name: 'Калининград',
